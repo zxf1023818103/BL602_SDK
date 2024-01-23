ee12837c92df00947c8e3fb9408b2523fee9b8d9
rfhuang <rfhuang@bouffalolab.com>
2024-01-23 10:22:27 +0800

    [WiFi]add the event for IPV6 Got

--- customer_app/bl602_demo_event/bl602_demo_event/main.c
+++ customer_app/bl602_demo_event/bl602_demo_event/main.c
@@ -516,6 +516,12 @@ static void event_cb_wifi_event(input_event_t *event, void *private_data)
             printf("[SYS] Memory left is %d Bytes\r\n", xPortGetFreeHeapSize());
         }
         break;
+        case CODE_WIFI_ON_GOT_IP6:
+        {
+            printf("[APP] [EVT] GOT IPv6 %lld\r\n", aos_now_ms());
+            printf("[SYS] Memory left is %d Bytes\r\n", xPortGetFreeHeapSize());
+        }
+        break;
         case CODE_WIFI_ON_PROV_SSID:
         {
             printf("[APP] [EVT] [PROV] [SSID] %lld: %s\r\n",
--- customer_app/bl602_demo_wifi/bl602_demo_wifi/main.c
+++ customer_app/bl602_demo_wifi/bl602_demo_wifi/main.c
@@ -398,6 +398,12 @@ static void event_cb_wifi_event(input_event_t *event, void *private_data)
             printf("[SYS] Memory left is %d Bytes\r\n", xPortGetFreeHeapSize());
         }
         break;
+        case CODE_WIFI_ON_GOT_IP6:
+        {
+            printf("[APP] [EVT] GOT IPv6 %lld\r\n", aos_now_ms());
+            printf("[SYS] Memory left is %d Bytes\r\n", xPortGetFreeHeapSize());
+        }
+        break;
         case CODE_WIFI_ON_EMERGENCY_MAC:
         {
             printf("[APP] [EVT] EMERGENCY MAC %lld\r\n", aos_now_ms());
